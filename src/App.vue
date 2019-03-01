f872ad9d11a38d4958188d51edbb127a9df0013a
helloxin <971137239@qq.com>
2019-03-01 16:47:19 +0800

    删除HelloWorld模板，创建Home模板

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="head">head</div>
+    <div id="content"></div>
+    <div id="foot"></div>
     <router-view/>
   </div>
 </template>
@@ -16,8 +18,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding:0px;
 }
 </style>
